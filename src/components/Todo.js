61d953797668538a732e57eeba8e1e28d441e10e
kabir-2323 <87745329+kabir-2323@users.noreply.github.com>
2021-10-14 23:11:05 +0530

    Add files via upload

---
+++ src/components/Todo.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import TodoForm from "./TodoForm";
+
+ import {    FaMarker } from "react-icons/fa";
+import { FaPrescriptionBottle } from "react-icons/fa";
+function Todo({ todos, completeTodo, removeTodo, updateTodo }) {
+  const [edit, setEdit] = useState({
+    // First Values
+    id: null,
+    value: "",
+  });
+
+  const submitUpdate = (value) => {
+    // Update todo and submit
+    updateTodo(edit.id, value);
+    setEdit({
+      id: null,
+      value: "",
+    });
+  };
+
+  if (edit.id) {
+    return <TodoForm edit={edit} onSubmit={submitUpdate} />; // Update !
+  }
+
+  return todos.map((todo, index) => (
+    <div
+      className={todo.isComplete ? "todo-row complete" : "todo-row"}
+      key={index}
+    >
+      <div key={todo.id} onClick={() => completeTodo(todo.id)}>
+        {todo.text}
+      </div>
+      <div className="icons">
+   
+        <   FaMarker
+          onClick={() => setEdit({ id: todo.id, value: todo.text })}
+          className="edit-icon"
+        />
+        <FaPrescriptionBottle
+          onClick={() => removeTodo(todo.id)}
+          className="delete-icon"
+        />
+      </div>
+    </div>
+  ));
+}
+
+export default Todo;
